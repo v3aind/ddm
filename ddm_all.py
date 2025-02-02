420774b3179a56f542be3123c0d420c6c73c0ab5
v3aind <111836704+v3aind@users.noreply.github.com>
2025-02-02 12:55:08 +0700

    Update ddm_all.py

--- ddm_all.py
+++ ddm_all.py
@@ -355,7 +355,7 @@ def process_files(file1, file2):
                                 "",
                                 "12200001178102" 
                             ],
-                            "Resultant Shortname": [""] * 3 + [f"{po_id_from_file1}:MRPRE00"] + [""] * 4 + [f"{po_id_from_file1}:MRPRE00"] + [""]
+                            "Resultant Shortname": [""] * 3 + ruleset_names[0] + [""] * 4 + ruleset_names[3] + [""]
                         }
 
                         rules_price_df = pd.DataFrame(rules_price_data)
